d9abc7e1d041d38b8a3d2a00ac1c8d493962d857
SSMN <serignesalioumbackend@gmail.com>
2024-11-06 23:12:43 +0000

    a

--- lib/bindings/auth_binding.dart
+++ lib/bindings/auth_binding.dart
@@ -1,11 +1,11 @@
-import 'package:get/get.dart';
-import 'package:learn_getx/controller/login_controller.dart';
-import 'package:learn_getx/controller/register_controller.dart';
+// import 'package:get/get.dart';
+// import 'package:learn_getx/controller/login_controller.dart';
+// import 'package:learn_getx/controller/register_controller.dart';
 
-class AuthBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(LoginController());
-    Get.put(RegisterController());
-  }
-}
+// class AuthBinding extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.put(LoginController());
+//     Get.put(RegisterController());
+//   }
+// }
